2c543929060791219c9d5bedbaec14f8c8e01b74
Liam Tengelis <liam@tengelisconsulting.com>
2021-01-02 19:08:19 -0800

    test db access

--- demo/demo_controllers/__init__.py
+++ demo/demo_controllers/__init__.py
@@ -1,13 +1,1 @@
-from webskeleton import db
-from webskeleton import routedef
-
-
-@routedef(method="GET", path="/big")
-async def big_route(req):
-    return {"hi": 3}
-
-
-@routedef(method="POST", path="/another", must_be_authenticated=False)
-async def another_route(req):
-    res = await db.fetch_val("select 1")
-    return {"hi": res}
+from .big_controller import *
