c87e79705a635048df9a98aa86b3d1e972899bb8
Liam Tengelis <liam@tengelisconsulting.com>
2021-02-09 19:38:21 -0800

    more tests of auth.py

--- test/webskeleton/auth.test.py
+++ test/webskeleton/auth.test.py
@@ -7,18 +7,28 @@ import jwt
 import webskeleton.auth as auth
 
 
+user_id = "test-user-id"
+
+
 class AuthTest(IsolatedAsyncioTestCase):
     def test_issue_access_token(self):
-        user_id = "test-user-id"
         token = auth.issue_access_token(user_id)
         return
 
     def test_issue_and_parse_creds(self):
-        user_id = "test-user-id"
         token = auth.issue_access_token(user_id)
         bearer_creds = f"Bearer {token}"
         claims = auth.creds_parse_bearer(bearer_creds)
         self.assertEqual(claims["user_id"], user_id)
+        return
+
+    async def test_issue_refresh_token(self):
+        import webskeleton.appredis as appredis
+        appredis.set_str = AsyncMock()
+        token = await auth.issue_refresh_token(user_id)
+        save_call = appredis.set_str.call_args_list[0]
+        self.assertEqual(save_call.args[0], token)
+        self.assertEqual(save_call.args[1], user_id)
         return
 
 
